888e9dd655b3c0634ccce6ba3691d17744cb3310
MrChickenBacon <34237142+MrChickenBacon@users.noreply.github.com>
2018-12-05 18:49:24 +0100

    More goodies!

--- Hotpink/Program.cs
+++ Hotpink/Program.cs
@@ -19,21 +19,38 @@ namespace Hotpink
             var user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(new Uri("https://i.imgur.com/YaryvoN.jpg"), $@"{user}\Documents\hp.jpg");
+                client.DownloadFile(new Uri("https://github.com/MrChickenBacon/HotPink/raw/master/Hotpink/cursor.cur"), $@"{user}\Documents\cursor.cur");
+                client.DownloadFile(new Uri("https://github.com/MrChickenBacon/HotPink/raw/master/Hotpink/Back.PNG"), $@"{user}\Documents\Back.PNG");
+                client.DownloadFile(new Uri("https://github.com/MrChickenBacon/HotPink/raw/master/Hotpink/hotpink.wav"), $@"{user}\Documents\hotpink.wav");
             }
 
-            var key = Registry.CurrentUser.CreateSubKey(@"AppEvents\Schemes\Apps\Explorer\Navigating\.Current");
-            var path = @"C:\Windows\media\tada.wav";
-            key?.SetValue("", path);
+            Thread.Sleep(1000);
 
             SoundPlayer player = new SoundPlayer("hotpink.wav");
             player.Play();
 
             Thread.Sleep(1000);
-            Process.Start("https://i.kym-cdn.com/photos/images/newsfeed/000/779/581/e7a.png");
+
+            //Sets new cursor
+            var key1 = Registry.CurrentUser.CreateSubKey(@"Control Panel\Cursors\Arrow");
+            var path1 = $@"{user}\Documents\cursor.cur";
+            key1?.SetValue("", path1);
+
+            Thread.Sleep(1000);
+            Process.Start("https://github.com/MrChickenBacon/HotPink/raw/master/Hotpink/Back.PNG");
+
+            Thread.Sleep(1000);
+            SystemParametersInfo(0x0014, 0, $@"{user}\Documents\Back.PNG", 0x0001);
+
+            
 
 
+            //Sets new sound on folder navigation
+            var key2 = Registry.CurrentUser.CreateSubKey(@"AppEvents\Schemes\Apps\Explorer\Navigating\.Current");
+            var path2 = $@"{user}\Documents\hotpink.wav";
+            key2?.SetValue("", path2);
 
+            Thread.Sleep(200000);
         }
     }
 }
